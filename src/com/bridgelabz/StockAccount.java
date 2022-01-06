9ff0867a1f5d9addc166caca97eeecf226b210b0
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-06 00:34:18 -0800

    Welcome To Stock Account Management Program

--- src/com/bridgelabz/StockAccount.java
+++ src/com/bridgelabz/StockAccount.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class StockAccount {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome To Stock Account Management Program");
-	}
-
+   System.out.println("Welcome To Stock Account Management Program");
 }
+}	
