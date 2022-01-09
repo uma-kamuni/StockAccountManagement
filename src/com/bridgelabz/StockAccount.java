cca14dcb0a5bd52b078d5ba2c54c09cc44ae19d8
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-09 12:14:08 -0800

    Added: created list of stock objects

--- src/com/bridgelabz/StockAccount.java
+++ src/com/bridgelabz/StockAccount.java
@@ -1,8 +1,41 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class StockAccount {
 
 	public static void main(String[] args) {
-   System.out.println("Welcome To Stock Account Management Program");
+    Scanner scannerObject = new Scanner(System.in);
+    Stock[] stockArray = new Stock[3];
+	int arrayIndex = 0;
+	Stock stockObject = new Stock();
+	StockOperations operationObject = new StockOperations();
+	
+	do {
+		System.out.println("1 : Add Stock\n2 : Value Of each stock\n3 : Total Value Of Stock");
+		int userChoice = scannerObject.nextInt();
+		switch(userChoice) {
+		case 1:
+			stockObject = operationObject.addStock();
+			stockArray[arrayIndex] = stockObject;
+			arrayIndex++;
+			break;
+
+		case 2:
+			
+			operationObject.calculateEachStockValue(stockArray);
+			break;
+			
+		case 3:
+			operationObject.calculateTotalValue(stockArray);
+			break;
+		}
+		
+		System.out.println("If you want to perform more Operations Enter true");
+		
+	}while(scannerObject.nextBoolean());
+	
+	scannerObject.close();
 }
-}	
+
+}
