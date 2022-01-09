0da0129edc894055ab7897ec96f71f011acf0be7
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-09 12:12:03 -0800

    Added: class to store Stock Details

---
+++ src/com/bridgelabz/Stock.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class Stock {
+	private String stockName;
+	private int price;
+	private int numberOfShare;
+	
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockNmae) {
+		this.stockName = stockNmae;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getNumberOfShare() {
+		return numberOfShare;
+	}
+	public void setNumberOfShare(int numberOfShare) {
+		this.numberOfShare = numberOfShare;
+	}
+	
+}
