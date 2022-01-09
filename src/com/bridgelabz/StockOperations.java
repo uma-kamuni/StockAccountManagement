b0c76098cb4d937b97c5db74713d6d2e45a91fac
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-09 12:15:54 -0800

    Added: methods to add stocks and calculate total value

---
+++ src/com/bridgelabz/StockOperations.java
@@ -0,0 +1,49 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class StockOperations {
+	Scanner sc = new Scanner(System.in);
+	
+	public Stock addStock() {
+		
+		Stock stock = new Stock();
+		
+		System.out.println("Enter the stock name:");
+		stock.setStockName(sc.next());
+		
+		System.out.println("Enter the price of Stock share:");
+		stock.setPrice(sc.nextInt());
+		
+		System.out.println("Enter the Number of Shares:");
+		stock.setNumberOfShare(sc.nextInt());
+		System.out.println(stock);
+		return stock;
+	
+	}
+	
+public void calculateEachStockValue(Stock[] stockArray) {
+		
+		for (int eachStock = 0; eachStock < stockArray.length; eachStock++) {
+			if(stockArray[eachStock] !=null) {
+				
+				Stock stock = stockArray[eachStock];
+				int value = stock.getPrice() * stock.getNumberOfShare();
+				System.out.println("The Value of " + stock.getStockName() + " is :" + value);
+			}
+			
+		}
+	}
+	
+	public void calculateTotalValue(Stock[] stockArray) {
+		int total =0;
+		for (int eachStock = 0; eachStock < stockArray.length; eachStock++) {
+			if(stockArray[eachStock] !=null) {
+				Stock stock = stockArray[eachStock];
+				total = total + (stock.getPrice()*stock.getNumberOfShare());
+			}
+		}
+		System.out.println("Total value of Stock is : "+total);
+	}
+	
+}
